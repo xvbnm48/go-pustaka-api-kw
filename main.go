20683bf82cd9068190597722d42de7b4ce28127d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-03-11 23:32:34 +0700

    make update,insert,and read data with gorm

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/xvbnm48/go-pustaka-api-kw/book"
+	book "github.com/xvbnm48/go-pustaka-api-kw/book"
 	"github.com/xvbnm48/go-pustaka-api-kw/handler"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -27,16 +28,35 @@ func main() {
 	}
 
 	db.AutoMigrate(&book.Book{})
-	book := &book.Book{}
-	book.Title = "Bloom into you"
-	book.Price = 150
-	book.Description = "bloom into you is a manga from japan about a 2 girls "
-	book.Rating = 9
+	// book := &book.Book{}
+	// book.Title = "Bloom into you"
+	// book.Price = 150
+	// book.Description = "bloom into you is a manga from japan about a 2 girls "
+	// book.Rating = 9
 
-	err = db.Create(&book).Error
+	// err = db.Create(&book).Error
+	// if err != nil {
+	// 	log.Fatal("db create error")
+	// }
+
+	// var book book.Book
+	var book book.Book
+	// err = db.Debug().First(&book, 2).Error
+	// err = db.Debug().Find(&books).Error
+	// err = db.Debug().Where("title =?", "Bloom into you").Find(&books).Error
+	err = db.Debug().Where("id =?", 1).First(&book).Error
 	if err != nil {
-		log.Fatal("db create error")
+		fmt.Println("error find book record ")
 	}
+
+	book.Title = "Golang books"
+	err = db.Save(&book).Error
+	if err != nil {
+		fmt.Println("error find book record ")
+	}
+
+	// fmt.Println("title:", book.Title)
+	// fmt.Println("book object %v", book)
 
 	router := gin.Default()
 
@@ -48,5 +68,10 @@ func main() {
 	v1.GET("/query", handler.QueryHandler)
 	v1.POST("/books", handler.PostBooksHandler)
 
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router.Run(":" + port)
+	// router.Run(":8080")
 }
